21c2da6f0a09dc2d7cc970d7f3e9556250f0d00c
FinkAr <a.fink@psych.uni-frankfurt.de>
2022-11-04 09:17:09 +0100

    Change comments

--- analyse.R
+++ analyse.R
@@ -11,18 +11,18 @@ package.check <- lapply(
 )
 #Funktionen laden
 source("functions/dotPlots.R")
-source("./functions/sw.R")
+source("functions/sw.R")
 source("functions/smith_waterman_compl.R")
 
-####Erselle Dotplots 
+####Erstelle Dotplots 
 
-#Settings für dotplots-Funktion mit Grundeinstellungen:
-##file = NULL; Input-File im csv oder xlsx-Format. Jeder Text muss in einer einzelnen Spalte dargestellt werden mit den Kürzeln für die Textabschnittejeweils in den Zellen darunter
+#Settings für dotplots-Funktion mit Voreinstellungen:
+##file = NULL; Input-File im csv oder xlsx-Format. Jeder Text muss in einer einzelnen Spalte dargestellt werden jeweils mit den Kürzeln für die Textabschnitte in den Zellen darunter
 ##sheet = wenn xlsx-File, dann Angabe des Excelsheets in der xlsx-Datei, dass die Daten enthält (Name oder Index möglich) 
 ##output.file = NULL; Name für Output-File im xlsx-Format
-##gap.marker = "GP"; Marker für Lücken im Input-File. Werden mit -1 in den Dotplots dargestellt. Werden intern automatisch durchnummeriert.
-##sep = F; Sollen alle Verlgeiche in einer großen Tabelle (sep =F) oder in separaten Tabellen pro Text (sep = T)dargestellt werden. Wenn sep = F dauert die Berechnung deutlich länger
-##colors = c("#ffa666","#ffd966", "#00BFFF") ; Farben für unterschiedliche KAtegorien im Dotplot im HEX-Format. Reihenfolge is Farbe für -1, Farbe für 0, Farbe für 1
+##gap.marker = "GP"; Marker für Lücken im Input-File; werden mit -1 in den Dotplots dargestellt; werden intern automatisch durchnummeriert.
+##sep = F; Sollen alle Vergleiche in einer großen Tabelle (sep = F) oder in separaten Tabellen pro Textsequenz (sep = T) dargestellt werden? Wenn sep = F dauert die Berechnung deutlich länger
+##colors = c("#ffa666","#ffd966", "#00BFFF") ; Farben für unterschiedliche Kategorien im Dotplot im HEX-Format. Reihenfolge ist Farbe für -1, Farbe für 0, Farbe für 1
 
 dotplots(file = "data/Priamelueberlieferung_reduziert.xlsx", sheet = 2, output.file = "results/dotplots_redux.xlsx", gap.marker = "GP", sep = F, colors = c("#ffa666","#ffd966", "#00BFFF")) 
 
@@ -32,8 +32,8 @@ dotplots(file = "data/Priamelueberlieferung_reduziert.xlsx", sheet = 2, output.f
 ##file = NULL; Input-File im csv oder xlsx-Format.
 ##sheet = wenn xlsx-File, dann Angabe des Excelsheets in der xlsx-Datei, dass die Daten enthält (Name oder Index möglich) 
 ##output.file = NULL; Name für Output-File im xlsx-Format
-##gap.marker = "GP"; Marker für Lücken im iInput-File. Werden intern automatisch durchnummeriert.
-##rev = T; Soll auch auch mit der invertierten Reihenfolge verglichen werden? Wir dgemacht wenn rev = T
-##sec_order = F; Second order Smith-Waterman -> Erneuter Vergleich der gefundenen Sequencen miteinander (wenn sec_order =T; Achtung: erhöht die Berehcnungszeit deutlich). 
+##gap.marker = "GP"; Marker für Lücken im Input-File. Werden intern automatisch durchnummeriert.
+##rev = T; Soll auch auch mit der invertierten Reihenfolge verglichen werden? Ja -> rev = T; nein -> rev = F
+##sec_order = F; second order Smith-Waterman;experminentell(!) -> Erneuter Vergleich der gefundenen Sequenzen miteinander (wenn sec_order = T; Achtung: erhöht die Berehcnungszeit deutlich). 
 
 sw(file = "data/Priamelueberlieferung_reduziert.xlsx", sheet = 2, output.file = "results/sw_incl_rev_redux.xlsx", gap.marker = "GP",rev = T, sec_order = F)
